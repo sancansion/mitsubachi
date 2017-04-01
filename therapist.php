e86d45f23e1180fe1bf7c1050254ec08090a0775
sancansion <sancansion@gmail.com>
2017-04-01 16:06:01 +0900

    therapist.php

--- therapist.php
+++ therapist.php
@@ -8,9 +8,9 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>セラピスト一覧 | Mitsubachi | ミツバチ | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 </title>
+<title>セラピスト一覧 | Mitsubachi | みつばち | メンズエステ | 大阪 | 天神橋六丁目 | 扇町 </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="author" content="Miss Mrs" />
+<meta name="author" content="Mitsubachi" />
 
 <meta name="description" content="Mitsubachi | ミツバチ | メンズエステ | 大阪 | 天神橋六丁目 | 扇町" />
 <meta name="keywords" content="Mitsubachi | ミツバチ | メンズエステ | 大阪 | 天神橋六丁目 | 扇町" />
@@ -104,15 +104,15 @@ for($i = $pager['index']; ($i-$pager['index']) < $pagelength; $i++){
 	$name = str_replace('<br />','',$lines_array[$i][2]);
 
 
-	if($lines_array[$i][7] == 1){
+	if($lines_array[$i][6] == 1){
 		$experience = "あり";
 	} else {
 		$experience = "なし";
 	}
 	
-	if($lines_array[$i][8] == 1){
+	if($lines_array[$i][7] == 1){
 		$looking = "スレンダー";
-	}else if($lines_array[$i][8] == 2){
+	}else if($lines_array[$i][7] == 2){
 		$looking = "普通";
 	} else {
 		$looking = "むっちり";
@@ -132,7 +132,7 @@ echo <<<EOF
 			<img src="{$img_updir}/thumb_{$lines_array[$i][0]}.{$lines_array[$i][3]}" alt="" style="display:none" />
 			<span class="case-studies-summary"> {$lines_array[$i][2]} ({$lines_array[$i][6]})</span>
 		</a>
-		<figcaption style="display:none;"> {$lines_array[$i][2]} 年齢：{$lines_array[$i][6]} / セラピスト経験：$experience / 体型： $looking</figcaption>
+		<figcaption style="display:none;"> {$lines_array[$i][2]} 年齢：{$lines_array[$i][5]} / セラピスト経験：$experience / 体型： $looking</figcaption>
 	</figure>
 	</li>
 
